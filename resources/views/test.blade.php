89542a22ca868a23026fcb4d23b3c0c1ff686884
abdo <a01226967080@gmail.com>
2024-12-12 21:28:17 +0200

    success install inertia

---
+++ resources/views/test.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<abbr title="World Wide Web">WWW</abbr>
+
+<img
+    alt="flower"
+    style="width: 100%"
+    src="https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=600"
+>
+<a href="https://laravel.com" target="laravel">Laravel</a>
+<div id="background">
+    <select class="select" name="select" style="width: 50%; height: 50%">
+        <option value="1">
+            Option 1
+        </option>
+        <option value="2">
+            Option 2
+        </option>
+        <option value="3">
+            Option 3
+        </option>
+    </select>
+</div>
+</body>
+</html>
